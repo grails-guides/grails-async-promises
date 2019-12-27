35b0dd974cbe71ae952228404d9f1bcb29241819
Sergio del Amo <sergio.delamo@softamo.com>
2019-12-27 08:24:01 +0100

    Update Ersatz

--- complete/grails-app/services/org/openweathermap/OpenweathermapService.groovy
+++ complete/grails-app/services/org/openweathermap/OpenweathermapService.groovy
@@ -1,75 +1,81 @@
 //tag::packageAndImports[]
 package org.openweathermap
 
-import grails.async.Promise
-import grails.async.PromiseList
-import io.micronaut.http.HttpStatus
-import io.micronaut.http.client.HttpClient
-import io.micronaut.http.HttpRequest
-import io.micronaut.http.HttpResponse
-import io.micronaut.http.uri.UriBuilder
-import org.grails.web.json.JSONObject
+import grails.config.Config
+import grails.core.support.GrailsConfigurationAware
 import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
-import org.springframework.beans.factory.annotation.Autowired
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.BlockingHttpClient
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.exceptions.HttpClientResponseException
+import io.micronaut.http.uri.UriBuilder
 
 //end::packageAndImports[]
 //tag::promisesImports[]
-import static grails.async.Promises.*
+import grails.async.Promise
+import grails.async.PromiseList
 //end::promisesImports[]
 
 //tag::grailsConfigurationAware[]
 @CompileStatic
-class OpenweathermapService {
+class OpenweathermapService implements GrailsConfigurationAware {
+    String appid
+    String cityName
+    String countryCode
+    BlockingHttpClient client
 
-    @Autowired OpenweathermapConfiguration openweathermapConfiguration
+    @Override
+    void setConfiguration(Config co) {
+        setupHttpClient(co.getProperty('openweather.url', String, 'http://api.openweathermap.org'))
+        appid = co.getProperty('openweather.appid', String)
+        cityName = co.getProperty('openweather.cityName', String)
+        countryCode = co.getProperty('openweather.countryCode', String)
+    }
+
+    void setupHttpClient(String url) {
+        this.client = HttpClient.create(url.toURL()).toBlocking()
+    }
 //end::grailsConfigurationAware[]
 
 //tag::currentWeather[]
-    @CompileDynamic
     CurrentWeather currentWeather(Unit units = Unit.Standard) {
-        currentWeather(openweathermapConfiguration.cityName, openweathermapConfiguration.countryCode, units)
+        currentWeather(cityName, countryCode, units)
     }
 
-    @CompileDynamic
     CurrentWeather currentWeather(String cityName, String countryCode, Unit unit = Unit.Standard) {
-        HttpClient client = HttpClient.create(openweathermapConfiguration.openWeatherUrl.toURL())
-        UriBuilder uriBuilder = UriBuilder.of('/data/2.5/weather')
-                .queryParam('q', "${cityName},${countryCode}".toString())
-                .queryParam('appid', openweathermapConfiguration.appid)
-        String unitParam = unitParameter(unit)
-        if (unitParam) {
-            uriBuilder = uriBuilder.queryParam('units', unitParam)
-        }
         try {
-            HttpResponse<Map> resp = client.toBlocking().exchange(HttpRequest.GET(uriBuilder.build()), Map)
-            if (resp.status == HttpStatus.OK && resp.body()) {
-                return OpenweathermapParser.currentWeatherFromJSONElement(new JSONObject(resp.body())) // <2>
-            }
-        } catch (Exception e) {
+            HttpRequest request = HttpRequest.GET(currentWeatherUri(cityName, countryCode, unit))
+            return client.retrieve(request, CurrentWeather)
+
+        } catch (HttpClientResponseException e) {
             return null // <3>
         }
     }
 
-  /**
-    * @return null if Standard Unit
-    */
-    String unitParameter(Unit unit)  {
-        switch ( unit ) {
-            case Unit.Metric:
-                return 'metric'
-            case Unit.Imperial:
-                return 'imperial'                
-            default:
-                return null
+    URI currentWeatherUri(String cityName, String countryCode, Unit unit = Unit.Standard) {
+        UriBuilder uriBuilder = UriBuilder.of('/data/2.5/weather')
+                .queryParam('q', "${cityName},${countryCode}".toString())
+                .queryParam('appid', appid)
+        String unitParam = unitParameter(unit)
+        if (unitParam) {
+            uriBuilder = uriBuilder.queryParam('units', unitParam)
         }
+        uriBuilder.build()
     }
+
+    String unitParameter(Unit unit)  {
+        unit == Unit.Standard ? null : unit?.toString()
+    }
+
+
 //end::currentWeather[]
 
 //tag::findCurrentWeatherByCitiesAndCountryCodeAsync[]
+    @CompileDynamic
     Promise<List<CurrentWeather>> findCurrentWeatherByCitiesAndCountryCodeWithPromises(List<String> cities, String countryCode, Unit unit) {
         PromiseList<CurrentWeather> list = new PromiseList<CurrentWeather>()
-        cities.each { String city -> 
+        cities.each { String city ->
             list << task { // <1>
                 currentWeather(city, countryCode, unit)
             }
@@ -77,9 +83,9 @@ class OpenweathermapService {
         return list // <2>
     }
 //end::findCurrentWeatherByCitiesAndCountryCodeAsync[]
-//tag::findCurrentWeatherByCitiesAndCountryCodeSynchronous[]    
+//tag::findCurrentWeatherByCitiesAndCountryCodeSynchronous[]
     List<CurrentWeather> findCurrentWeatherByCitiesAndCountryCode(List<String> cities, String countryCode, Unit unit) {
-        cities.collect { currentWeather(it, countryCode, unit) }    
+        cities.collect { currentWeather(it, countryCode, unit) }
     }
-//end::findCurrentWeatherByCitiesAndCountryCodeSynchronous[]    
+//end::findCurrentWeatherByCitiesAndCountryCodeSynchronous[]
 }
--- initial/src/main/groovy/org/openweathermap/Clouds.groovy
+++
@@ -1,5 +0,0 @@
-package org.openweathermap
-
-class Clouds {
-    Integer cloudiness
-}
